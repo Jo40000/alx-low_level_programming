90110871510f11b201091085923d16a537fe0298
Joseph Nathan <jbaynik23@gmail.com>
2022-04-07 09:22:40 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - prints the "Programmiing is like buiding a multilingual puzzle
+ * 
+ * Return: 0 if exited properly, no-zero otherwise
+ *
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
